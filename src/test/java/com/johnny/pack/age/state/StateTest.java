16cb7c84cad3f4a52f90783ed25768588494fad1
galvelistious37 <galvelistious37@gmail.com>
2022-10-05 19:59:33 -0700

    StateTest.java updated with test cases

--- src/test/java/com/johnny/pack/age/state/StateTest.java
+++ src/test/java/com/johnny/pack/age/state/StateTest.java
@@ -1,7 +1,26 @@
 package com.johnny.pack.age.state;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StateTest {
+    private State objectUnderTest;
+
+    @BeforeEach
+    void setup(){
+        objectUnderTest = new State();
+    }
+
+    @Test
+    @DisplayName("State Getter/Setter and toString")
+    void stateGetterSetterAndToString_test(){
+        objectUnderTest.setStateId(1);
+        objectUnderTest.setName("OR");
+        String expected = "1, OR";
+        assertEquals(expected, objectUnderTest.toString());
+    }
 
 }
